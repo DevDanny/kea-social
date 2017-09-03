06232b41578f109d493ca51443c1884cf3f53ffb
Danny <danny.hansen@me.com>
2017-09-03 13:46:29 +0200

    Initial Commit

---
+++ kea-social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kea-social
+//
+//  Created by Danny Hansen on 03/09/2017.
+//  Copyright © 2017 Danny Olsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
